9b97037974b08832a4e79c8d18cd136d692715fb
Ezeki Zibzibadze <ezeki@jok.ge>
2013-05-07 01:18:47 +0400

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ try {
         var container = document.getElementById('pagelet_ego_pane_w').children[0].children[0];
         var lastChild = container.lastChild
         var node = document.createElement('div');
-        node.innerText = 'Hello FB :))';
+        node.innerHTML = '<iframe src="http://localhost:9000/Portal/FacebookAdsFrame" style="border: 0px; width: 244px; overflow: hidden;"></iframe>';
 
         container.insertBefore(node, lastChild);
         //chrome.extension.sendRequest(request, function(response) {});
