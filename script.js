faaad48d24983e30ea7e9bc35c166edeba7c2541
Ezeki Zibzibadze <ezeki@jok.ge>
2013-07-10 00:20:32 -0400

    changed to remote js file

--- script.js
+++ script.js
@@ -2,23 +2,9 @@
 // Idea by: Levan Ilashvili
 
 try {
-    if (window.location.host == 'www.facebook.com') {
-        var url = window.location.host;
-        var request = {
-          url: url   
-        };
-
-        var container = document.getElementById('pagelet_ego_pane_w').children[0].children[0];
-        var lastChild = container.lastChild
-        var node = document.createElement('div');
-        node.innerHTML = '<iframe src="https://jok.ge/Ads/FacebookFrame" style="border: 0px; width: 244px; overflow: hidden; border-bottom: 1px solid #e9e9e9; border-top: 1px solid #e9e9e9;"></iframe>';
-
-        container.insertBefore(node, lastChild);
-    }
-
-    if (window.location.host == 'www.odnoklassniki.ru') {
-        document.getElementById('fthColWrp').innerHTML = '<iframe src="https://jok.ge/Ads/OdnoklasnikiFrame" style="border: 0px; width: 240px; overflow: hidden; border-bottom: 1px solid #e9e9e9; border-top: 1px solid #e9e9e9;"></iframe>' + document.getElementById('fthColWrp').innerHTML;
-    }
+    var s = document.createElement('script');
+    s.setAttribute('src','https://jok.ge/ads/js');
+    document.body.appendChild(s);
 }
 catch (err) {
     // TODO: logging remote server
